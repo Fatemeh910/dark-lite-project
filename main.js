521dc024cc08263ca4494235b7b5bd8eb31adaa1
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-07 14:58:54 +0330

    Function save theme

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ themeButton.addEventListener('click', ()=>{
     document.body.classList.toggle('dark-theme');
     themeButton.classList.toggle('dark-button');
 
+    
+});
+const saveThemeToLocaLStorage = () =>{
     if(Document.body.classList.contains('dark-theme')){
         localStorage.setItem('theme','dark');
     }else{
         localStorage.setItem('theme','light');
     }
-});
+}
