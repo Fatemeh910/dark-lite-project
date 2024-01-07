03aadf1532f893e25c0a2a746a8d5c09a75c70e5
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-07 14:53:42 +0330

    Set kardane local storage

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ const themeButton = document.querySelector(".theme-button");
 themeButton.addEventListener('click', ()=>{
     document.body.classList.toggle('dark-theme');
     themeButton.classList.toggle('dark-button');
-})
+
+    if(Document.body.classList.contains('dark-theme')){
+        localStorage.setItem('theme','dark');
+    }else{
+        localStorage.setItem('theme','dark');
+    }
+});
