3fb05a7ff6fbb6b218ee842d1b3ef8432fba5ff7
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-07 14:34:35 +0330

    Toggle kardane clas list theme button

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ const themeButton = document.querySelector(".theme-button");
 
 themeButton.addEventListener('click', ()=>{
     document.body.classList.toggle('dark-theme');
+    themeButton.classList.toggle('dark-button');
 })
