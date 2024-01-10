2056d05cf37dca51b8c51d48c5a2799dd78c2ce2
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-10 15:03:03 +0330

    Taaviz line

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 const themeButton = document.querySelector(".theme-button");
-    window.addEventListener('DOMContentLoaded',() => {
-       
-    });
 
-
-
-
-themeButton.addEventListener('click', ()=>{
-    document.body.classList.toggle('dark-theme');
-    themeButton.classList.toggle('dark-button');
+themeButton.addEventListener("click", ()=>  {
+    document.body.classList.toggle("dark-theme");
+    themeButton.classList.toggle("dark-button");
 
     saveThemeToLocaLStorage();
 });
+
 // مااخرین وضعیت تم راتوی لوکال  استوریج ذخیره کردیم
 const saveThemeToLocaLStorage = () =>{
-    if(Document.body.classList.contains('dark-theme')){
-        localStorage.setItem('theme','dark');
-    }else{
-        localStorage.setItem('theme','light');
+    if(document.body.classList.contains("dark-theme")){
+        localStorage.setItem("theme","dark");
+    } else {
+        localStorage.setItem("theme","light");
     }
 };
+//اپدیت تم زمانی کهد سایت لود میشود
 
 const updateTheme = () => {
-    if(localStorage.getItem('theme') === "dark"){
+    if (LocalStorage.getItem("theme") === "dark"){
         document.body.classList.add("dark-theme");
         themeButton.classList.add("dark-button");
-      }else{
+      } else {
           document.body.classList.remove("dark-theme");
           themeButton.classList.remove("dark-button");
       }
 };
+window.addEventListener("DOMContentLoaded",updateTheme);
